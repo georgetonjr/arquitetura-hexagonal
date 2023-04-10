b603dca5aa3e2639b0af7e9dd2a300bdfba7629c
Georgeton Junior <georgetonjr@gmail.com>
2023-04-10 13:59:58 -0300

    feat: add get method and test on productDb

--- adapters/db/product.go
+++ adapters/db/product.go
@@ -1,24 +1,28 @@
 package db
 
 import (
-	"github.com/georgetonjr/arquitetura-hexagonal/application"
-  "context"
   "database/sql"
+
   _ "github.com/mattn/go-sqlite3"
+
+	"github.com/georgetonjr/arquitetura-hexagonal/application"
 )
 
 type ProductDb struct {
 	db *sql.DB
 }
 
+func NewProductDb(db *sql.DB) *ProductDb {
+	return &ProductDb{db: db}
+}
+
 func (p *ProductDb) Get(id string) (application.ProductInterface, error) {
 	var product application.Product
-	stmt, err := p.db.Prepare("select id, name, price, status from products where product id=?")
+	stmt, err := p.db.Prepare("select id, name, price, status from products where id=?")
 	if err != nil {
 		return nil, err
 	}
-
-	err = stmt.QueryRow(id).Scan(&product.Id, &product.Name, &product.Price, &product.Status)
+	err = stmt.QueryRow(id).Scan(&product.ID, &product.Name, &product.Price, &product.Status)
 	if err != nil {
 		return nil, err
 	}
